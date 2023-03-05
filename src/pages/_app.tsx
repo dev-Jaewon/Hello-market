2d5614bedefd4067d7610abb87a6e14dc8097bdf
dev-Jaewon <app235naver@gmail.com>
2023-03-06 01:49:54 +0900

    [init] project 세팅

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { AppProps } from "next/app";
+
+import "../styles/global.css";
+
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    return (
+        <>
+            <Component {...pageProps} />
+        </>
+    );
+}
