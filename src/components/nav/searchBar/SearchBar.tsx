43a2e7e3b57bfc57397455ad9a4a1b27413e49f5
dev-Jaewon <app235naver@gmail.com>
2023-03-13 01:34:08 +0900

    [feat] - nav 검색바

---
+++ src/components/nav/searchBar/SearchBar.tsx
@@ -0,0 +1,70 @@
+import styled from "@emotion/styled";
+import { ChangeEvent, useRef, useState } from "react";
+import { SlMagnifier } from "react-icons/sl";
+import { AiFillCloseCircle } from "react-icons/ai";
+
+export const SearchBar = () => {
+    const [inputValue, setInputValue] = useState<string>("");
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleEraserButtonClick = () => {
+        setInputValue("");
+    };
+
+    return (
+        <SearchBarCointaer>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="검색어를 입력해주세요"
+            />
+            {inputValue && (
+                <i
+                    role="eraser"
+                    className="eraser"
+                    onClick={handleEraserButtonClick}
+                >
+                    <AiFillCloseCircle />
+                </i>
+            )}
+
+            <i role="magnifier">
+                <SlMagnifier size={"20px"} />
+            </i>
+        </SearchBarCointaer>
+    );
+};
+
+const SearchBarCointaer = styled.div`
+    display: flex;
+    align-items: center;
+    padding-left: 14px;
+    width: 400px;
+    height: 48px;
+    border: 1px solid var(--brand);
+    border-radius: 4px;
+
+    .eraser {
+        color: #c3c0c0;
+        cursor: pointer;
+    }
+
+    input {
+        flex: 1;
+        border: unset;
+        margin-right: 14px;
+        font-size: 16px;
+
+        &:focus {
+            outline: none;
+        }
+    }
+
+    i {
+        width: 30px;
+    }
+`;
