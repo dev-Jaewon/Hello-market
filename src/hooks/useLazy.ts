4c2e2b53cd292c0177cff1d859df40762a695f65
dev-Jaewon <app235naver@gmail.com>
2023-03-20 04:55:14 +0900

    [feat] - 레이지 로딩

---
+++ src/hooks/useLazy.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useLazy = () => {
+    const ref = useRef<null | HTMLElement>(null);
+    const [inViewPort, setInViewPort] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (!ref.current) return;
+
+        const observer = new IntersectionObserver((entries) => {
+            const [entry] = entries;
+            if (entry.isIntersecting) {
+                setInViewPort(true);
+
+                if (ref.current) observer.unobserve(ref.current);
+            }
+        }, {});
+
+        observer.observe(ref.current);
+
+        () => {
+            if (ref.current) observer.unobserve(ref.current);
+        };
+    }, []);
+
+    return { ref, inViewPort };
+};
